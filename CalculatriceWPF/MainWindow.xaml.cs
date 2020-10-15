a5a95fa9f56b27428054d61cd9e217748989626e
nkerzazi <nkerzazi@gmail.com>
2020-10-15 17:06:11 -0400

    code C sharp

--- CalculatriceWPF/MainWindow.xaml.cs
+++ CalculatriceWPF/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace CalculatriceWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Double resultCalcul = 0;
+        string typeOperation = "";
+        bool isOperationCliked = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,9 +30,91 @@ namespace CalculatriceWPF
 
         private void bouttonChiffre_Click(object sender, RoutedEventArgs e)
         {
+            if ((txt_Resultat.Text == "0") || (isOperationCliked))
+                txt_Resultat.Clear();
+
+
             Button b = (Button)sender;
-            txt_Resultat.Content += b.Content.ToString();
-            
+
+            if (b.Content.ToString() == ",")
+            {
+                if (!txt_Resultat.Text.Contains(","))
+                {
+                    txt_Resultat.Text += b.Content.ToString();
+                    labelCurrentOperation.Content = txt_Resultat.Text;
+                }
+                
+            }
+            else
+            {
+                txt_Resultat.Text += b.Content.ToString();
+                labelCurrentOperation.Content += b.Content.ToString();
+
+            }
+
+
+        }
+
+        private void btn_InvSigne_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txt_Resultat.Text.Contains("-"))
+                txt_Resultat.Text = "-" + txt_Resultat.Text;
+            else
+                txt_Resultat.Text = txt_Resultat.Text.Substring(1, txt_Resultat.Text.Length-1);
+        }
+
+        private void Operateur_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            if (resultCalcul !=0)
+            {
+                typeOperation = b.Content.ToString();
+                isOperationCliked = true;
+                labelCurrentOperation.Content += typeOperation;
+            }
+            else
+            {
+                typeOperation = b.Content.ToString();
+                resultCalcul = Double.Parse(txt_Resultat.Text);
+                labelCurrentOperation.Content = resultCalcul + " " + typeOperation; 
+                isOperationCliked = true;
+            }
+        }
+
+        private void btn_C_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Resultat.Text = "0";
+            labelCurrentOperation.Content = "";
+            resultCalcul = 0;
+            typeOperation = "";
+            isOperationCliked = false;
+
+        }
+
+        private void egale_Click(object sender, RoutedEventArgs e)
+        {
+            switch(typeOperation)
+            {
+                case "+":
+                    txt_Resultat.Text = (resultCalcul + Double.Parse(txt_Resultat.Text)).ToString();
+                    break;
+                case "-":
+                    txt_Resultat.Text = (resultCalcul - Double.Parse(txt_Resultat.Text)).ToString();
+                    break;
+                case "*":
+                    txt_Resultat.Text = (resultCalcul * Double.Parse(txt_Resultat.Text)).ToString();
+                    break;
+                case "/":
+                    txt_Resultat.Text = (resultCalcul / Double.Parse(txt_Resultat.Text)).ToString();
+                    break;
+
+                //default:
+                //    break;
+            }
+            resultCalcul = Double.Parse(txt_Resultat.Text);
+            labelCurrentOperation.Content += "=" + resultCalcul;
+
+
         }
     }
 }
